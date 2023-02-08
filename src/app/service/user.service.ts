d8643f167c783705fd92ec28e1de2f42182c657e
nazimgueye <nazimgueye17@gmail.com>
2023-02-08 15:12:07 +0000

    topbar

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -11,15 +11,24 @@ export class UserService {
   baseUrl = environment.urlApi;
   constructor(private httpClient: HttpClient) { }
 
-  getUserByUsername(email):Observable<any> {
-    console.log(this.baseUrl);
-    
-    console.log(this.httpClient.get(this.baseUrl + '/user/email/' + email));
-    
-    return this.httpClient.get(this.baseUrl + '/user/email/' + email);
+  getUserByUsername(userName):Observable<any> {
+    console.log(this.baseUrl);    
+    return this.httpClient.get(this.baseUrl + "/api/user"  + userName);
 }
 
 getUserById(id:number):Observable<any> {
     return this.httpClient.get(this.baseUrl + '/user/' + id);
+}
+
+addUser(user){
+  return this.httpClient.post(this.baseUrl + '/user',user )
+}
+
+updateUser(user){
+  // return this.httpClient.post(this.baseUrl + '/user',user )
+
+}
+deleteUser(id){
+  return this.httpClient.delete(this.baseUrl + '/user',id )
 }
 }
