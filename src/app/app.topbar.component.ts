3ec73067fb47920bc33bb2df551e71f6756d0575
nazimgueye <nazimgueye17@gmail.com>
2023-02-08 12:08:49 +0000

    keycloak

--- src/app/app.topbar.component.ts
+++ src/app/app.topbar.component.ts
@@ -2,9 +2,6 @@ import { Component, OnDestroy } from '@angular/core';
 import { AppMainComponent } from './app.main.component';
 import { Subscription } from 'rxjs';
 import { MenuItem } from 'primeng/api';
-import { KeycloakService } from 'keycloak-angular';
-import { environment } from 'src/environments/environment';
-import { UserService } from './service/user.service';
 
 @Component({
     selector: 'app-topbar',
@@ -13,26 +10,6 @@ import { UserService } from './service/user.service';
 export class AppTopBarComponent {
 
     items: MenuItem[];
-    user
 
-    constructor(public appMain: AppMainComponent,private userService: UserService,
-        public keycloak: KeycloakService) { 
-            this.keycloak.loadUserProfile().then( res => {
-                console.log(res);
-                this.user = res;
-                this.getUserByUsername(res.username );
-            })
-        }
-
-    public getUserByUsername(username){
-        console.log(username);
-        return this.userService.getUserByUsername(username).subscribe(data => {
-            console.log(data);
-            this.user = data;
-        })
-    }
-
-    public deconnexion() {
-        return this.keycloak.logout();
-    }
+    constructor(public appMain: AppMainComponent) { }
 }
